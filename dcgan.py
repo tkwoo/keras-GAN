ab6f59c6b567e759f572a4fb3d7650b03dbfda5a
tkwoo <wtk1101@gmail.com>
2018-01-31 16:48:13 +0900

    [update] It finally working T.T

--- dcgan.py
+++ dcgan.py
@@ -1,13 +1,18 @@
+from __future__ import print_function
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.models import Model
+from keras.layers import Dense, Input
 from keras.layers import Reshape
 from keras.layers.core import Activation
 from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import UpSampling2D
-from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers import UpSampling2D
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2DTranspose
 from keras.layers.core import Flatten
 from keras.optimizers import SGD, Adam, RMSprop
 from keras.datasets import mnist
+from keras.layers import LeakyReLU
+from keras.utils. generic_utils import Progbar
 import numpy as np
 from PIL import Image
 import argparse
@@ -23,39 +28,57 @@ config.gpu_options.allow_growth = True
 set_session(tf.Session(config=config))
 
 def generator_model():
-    model = Sequential()
-    model.add(Dense(input_dim=100, units=1024))
-    model.add(Activation('tanh'))
-    model.add(Dense(128*7*7))
-    model.add(BatchNormalization())
-    model.add(Activation('tanh'))
-    model.add(Reshape((7, 7, 128), input_shape=(128*7*7,)))
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(Conv2D(64, (5, 5), padding='same'))
-    model.add(Activation('tanh'))
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(Conv2D(1, (5, 5), padding='same'))
-    model.add(Activation('tanh'))
+    # model = Sequential()
+    inputs = Input((100,))
+    fc1 = Dense(input_dim=100, units=128*7*7)(inputs)
+    fc1 = BatchNormalization()(fc1)
+    # fc1 = Activation('relu')(fc1)
+    fc1 = LeakyReLU(0.2)(fc1)
+    # fc2 = Dense(128*7*7)(fc1)
+    # fc2 = BatchNormalization()(fc2)
+    # fc2 = Activation('relu')(fc2)
+    fc2 = Reshape((7, 7, 128), input_shape=(128*7*7,))(fc1)
+    
+    up1 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(fc2)
+    
+    # up1 = BatchNormalization()(up1)
+    # up1 = Activation('relu')(up1)
+    conv1 = Conv2D(64, (3, 3), padding='same')(up1)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Activation('relu')(conv1)
+    up2 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv1)
+    # up2 = BatchNormalization()(up2)
+    # up2 = Activation('relu')(up2)
+    conv2 = Conv2D(1, (5, 5), padding='same')(up2)
+    output = Activation('tanh')(conv2)
+    
+    model = Model(inputs=[inputs], outputs=[output])
+    # model.summary()
+    # exit()
     return model
 
 
 def discriminator_model():
     model = Sequential()
-    model.add(
-            Conv2D(64, (5, 5),
-            padding='same',
-            input_shape=(28, 28, 1))
-            )
-    model.add(Activation('tanh'))
+    model.add(Conv2D(64, (5, 5), padding='same', input_shape=(28, 28, 1)))
+    # model.add(Activation('tanh'))
+    model.add(LeakyReLU(0.2))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(128, (5, 5)))
-    model.add(Activation('tanh'))
+    model.add(Conv2D(128, (5, 5), padding='same'))
+    # model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+    # model.add(Activation('tanh'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(Activation('tanh'))
+    # model.add(Dense(1024))
+    # model.add(BatchNormalization())
+    # model.add(LeakyReLU(0.2))
+    # model.add(Activation('tanh'))
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
+    # model.summary()
+    # exit()
     return model
 
 
@@ -95,15 +118,19 @@ def train(BATCH_SIZE):
     g = generator_model()
     d_on_g = generator_containing_discriminator(g, d)
     #SGD(lr=0.0005, momentum=0.9, nesterov=True)
-    d_optim = RMSprop()#(lr=0.001, decay=1e-6)
-    g_optim = RMSprop()#(lr=0.001, decay=1e-6)
-    g.compile(loss='binary_crossentropy', optimizer=g_optim)#"SGD")
+    #RMSprop() #Adam(lr=0.0002, decay=1e-6)
+    d_optim = RMSprop()
+    g_optim = RMSprop(lr=0.0002)
+    g.compile(loss='binary_crossentropy', optimizer=g_optim)
     d_on_g.compile(loss='binary_crossentropy', optimizer=g_optim)
     d.trainable = True
     d.compile(loss='binary_crossentropy', optimizer=d_optim)
     for epoch in range(100):
         print("Epoch is", epoch)
         print("Number of batches", int(X_train.shape[0]/BATCH_SIZE))
+        n_iter = int(X_train.shape[0]/BATCH_SIZE)
+        progress_bar = Progbar(target=n_iter)
+
         for index in range(int(X_train.shape[0]/BATCH_SIZE)):
             noise = np.random.uniform(-1, 1, size=(BATCH_SIZE, 100))
             image_batch = X_train[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
@@ -111,20 +138,28 @@ def train(BATCH_SIZE):
             if index % 20 == 0:
                 image = combine_images(generated_images)
                 image = image*127.5+127.5
-                Image.fromarray(image.astype(np.uint8)).save(
+                Image.fromarray(image.astype(np.uint8)).save('./result/'+
                     str(epoch)+"_"+str(index)+".png")
             X = np.concatenate((image_batch, generated_images))
             y = [1] * BATCH_SIZE + [0] * BATCH_SIZE
-            d_loss = d.train_on_batch(X, y)
-            print("batch %d d_loss : %f" % (index, d_loss))
+            if epoch >= 2 and  index%2 == 0:
+                d_loss = d.train_on_batch(X, y)
+            elif epoch < 2:
+                d_loss = d.train_on_batch(X, y)
+            else:
+                d_loss = d_loss
+            # print("batch %d d_loss : %f" % (index, d_loss))
             noise = np.random.uniform(-1, 1, (BATCH_SIZE, 100))
             d.trainable = False
             g_loss = d_on_g.train_on_batch(noise, [1] * BATCH_SIZE)
             d.trainable = True
-            print("batch %d g_loss : %f" % (index, g_loss))
+            # print("batch %d g_loss : %f" % (index, g_loss))
+            # progress_bar.add(n_iter, values=[('g',g_loss), ('d',d_loss)])
+            progress_bar.update(index, values=[('g',g_loss), ('d',d_loss)])
             if index % 10 == 9:
                 g.save_weights('generator.h5', True)
                 d.save_weights('discriminator.h5', True)
+        print ('')
 
 
 def generate(BATCH_SIZE, nice=False):
@@ -159,7 +194,7 @@ def generate(BATCH_SIZE, nice=False):
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mode", type=str)
+    parser.add_argument("--mode", type=str, default='train')
     parser.add_argument("--batch_size", type=int, default=128)
     parser.add_argument("--nice", dest="nice", action="store_true")
     parser.set_defaults(nice=False)
